2dba1d4fdb2a4b87eab7d74e5d5ca0e428face6e
unknown <hpakridge@hpakridge.austin.vauto.com>
2015-01-08 15:32:17 -0600

    Fixed generated by with custom generation ids

--- Converter/Methods/GeneratedBy.cs
+++ Converter/Methods/GeneratedBy.cs
@@ -3,69 +3,74 @@ using NHibernate.Cfg.MappingSchema;
 
 namespace NHibernateHbmToFluent.Converter.Methods
 {
-	public class GeneratedBy
-	{
-		private readonly CodeFileBuilder _builder;
+    public class GeneratedBy
+    {
+        private readonly CodeFileBuilder _builder;
 
-		public GeneratedBy(CodeFileBuilder builder)
-		{
-			_builder = builder;
-		}
+        public GeneratedBy(CodeFileBuilder builder)
+        {
+            _builder = builder;
+        }
 
-		public void Add(MappedPropertyInfo info)
-		{
-			if (info.Type != PropertyMappingType.Id)
-			{
-				return;
-			}
+        public void Add(MappedPropertyInfo info)
+        {
+            if (info.Type != PropertyMappingType.Id)
+            {
+                return;
+            }
 
-			HbmId id = info.HbmObject<HbmId>();
-			HbmGenerator generator = id.generator;
-			if (generator != null)
-			{
-				switch (generator.@class)
-				{
-					case "sequence":
-						{
-							HbmParam[] parameters = generator.param;
-							string[] text = parameters[0].Text;
-							_builder.AddLine(string.Format(".{0}.{1}(\"{2}\")", FluentNHibernateNames.GeneratedBy, FluentNHibernateNames.Sequence, text[0]));
-							break;
-						}
-					case "assigned":
-						_builder.AddLine(string.Format(".{0}.{1}()", FluentNHibernateNames.GeneratedBy, FluentNHibernateNames.Assigned));
-						break;
-					case "native":
-						_builder.AddLine(string.Format(".{0}.{1}()", FluentNHibernateNames.GeneratedBy, FluentNHibernateNames.Native));
-						break;
-					default:
-						_builder.AddLine(string.Format(".{0}. ?", FluentNHibernateNames.GeneratedBy));
-						break;
-				}
-			}
-		}
+            HbmId id = info.HbmObject<HbmId>();
+            HbmGenerator generator = id.generator;
+            if (generator != null)
+            {
+                switch (generator.@class)
+                {
+                    case "sequence":
+                        {
+                            HbmParam[] parameters = generator.param;
+                            string[] text = parameters[0].Text;
+                            _builder.AddLine(string.Format(".{0}.{1}(\"{2}\")", FluentNHibernateNames.GeneratedBy, FluentNHibernateNames.Sequence, text[0]));
+                            break;
+                        }
+                    case "assigned":
+                        _builder.AddLine(string.Format(".{0}.{1}()", FluentNHibernateNames.GeneratedBy, FluentNHibernateNames.Assigned));
+                        break;
+                    case "native":
+                        _builder.AddLine(string.Format(".{0}.{1}()", FluentNHibernateNames.GeneratedBy, FluentNHibernateNames.Native));
+                        break;
+                    default:
+                        _builder.AddLine(string.Format(".{0}.{1}", FluentNHibernateNames.GeneratedBy, FluentNHibernateNames.Custom(generator.@class)));
+                        break;
+                }
+            }
+        }
 
-		public static class FluentNHibernateNames
-		{
-			public static string GeneratedBy
-			{
-				get { return ReflectionUtility.GetPropertyName((IdentityPart ip) => ip.GeneratedBy); }
-			}
+        public static class FluentNHibernateNames
+        {
+            public static string GeneratedBy
+            {
+                get { return ReflectionUtility.GetPropertyName((IdentityPart ip) => ip.GeneratedBy); }
+            }
 
-			public static string Assigned
-			{
-				get { return ReflectionUtility.GetMethodName((IdentityPart ip) => ip.GeneratedBy.Assigned()); }
-			}
+            public static string Assigned
+            {
+                get { return ReflectionUtility.GetMethodName((IdentityPart ip) => ip.GeneratedBy.Assigned()); }
+            }
 
-			public static string Native
-			{
-				get { return ReflectionUtility.GetMethodName((IdentityPart ip) => ip.GeneratedBy.Native()); }
-			}
+            public static string Native
+            {
+                get { return ReflectionUtility.GetMethodName((IdentityPart ip) => ip.GeneratedBy.Native()); }
+            }
 
-			public static string Sequence
-			{
-				get { return ReflectionUtility.GetMethodName((IdentityPart ip) => ip.GeneratedBy.Sequence(null)); }
-			}
-		}
-	}
+            public static string Sequence
+            {
+                get { return ReflectionUtility.GetMethodName((IdentityPart ip) => ip.GeneratedBy.Sequence(null)); }
+            }
+
+            public static string Custom(string customName)
+            {
+                return ReflectionUtility.GetMethodName((IdentityPart ip) => ip.GeneratedBy.Custom(customName)) + string.Format("(\"{0}\")", customName);
+            }
+        }
+    }
 }
